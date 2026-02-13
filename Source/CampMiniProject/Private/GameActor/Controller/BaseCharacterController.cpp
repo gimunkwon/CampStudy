e325270e1323de90ff9d4b7db0a26af7f98a42e4
gimunkwon <gimon71@gmail.com>
2026-02-13 13:04:49 +0900

    Feat: InputMappingContext 플레이어의 Subsystem에 AddMappingContext및 EnhacnedInputComponent에 이동에 관련된 함수 OnInputClickToMove를 바인딩

---
+++ Source/CampMiniProject/Private/GameActor/Controller/BaseCharacterController.cpp
@@ -0,0 +1,47 @@
+﻿#include "GameActor/Controller/BaseCharacterController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameActor/Character/BaseCharacter.h"
+
+ABaseCharacterController::ABaseCharacterController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
+
+void ABaseCharacterController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		SubSystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+	
+}
+
+void ABaseCharacterController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (UEnhancedInputComponent* EInputComp = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EInputComp->BindAction(IA_ClickToMove, ETriggerEvent::Started, this, &ABaseCharacterController::OnInputClickToMove);
+		EInputComp->BindAction(IA_ClickToMove, ETriggerEvent::Triggered, this, &ABaseCharacterController::OnInputClickToMove);
+	}
+}
+
+void ABaseCharacterController::OnInputClickToMove()
+{
+	FHitResult Hit;
+	
+	if (GetHitResultUnderCursor(ECC_Visibility, 0, Hit))
+	{
+		if (ABaseCharacter* GPlayer = Cast<ABaseCharacter>(GetPawn()))
+		{
+			GPlayer->MoveToLocation(Hit.Location);
+		}
+		
+	}
+}
---
+++ Source/CampMiniProject/Public/GameActor/Controller/BaseCharacterController.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseCharacterController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+
+UCLASS()
+class CAMPMINIPROJECT_API ABaseCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ABaseCharacterController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+#pragma region InputMapping,Action
+	/*InputAction,InputMapping*/
+	UPROPERTY(EditAnywhere,Category="Input| InputMapping")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	UPROPERTY(EditAnywhere,Category="Input| InputAction")
+	TObjectPtr<UInputAction> IA_ClickToMove;
+	
+	/*-------Functions-------*/
+	void OnInputClickToMove();
+	
+#pragma endregion 
+};
