d6b9eb76936cf140770313fd14d8c02a5c06cb4e
gimunkwon <gimon71@gmail.com>
2026-02-13 17:24:44 +0900

    Feat: HUD클래스에서 NPCWidget의 생성및 제거 로직 추가

---
+++ Source/CampMiniProject/Public/UI/HUD/MiniHUD.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MiniHUD.generated.h"
+
+
+class UNPCWidget;
+
+UCLASS()
+class CAMPMINIPROJECT_API AMiniHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	void ShowNPCWidget();
+	
+protected:
+	UPROPERTY(EditAnywhere,Category="Widget|NPC")
+	TSubclassOf<UUserWidget> NPCWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UNPCWidget> NPCWidget;
+};
