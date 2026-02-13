b68876edaf4884eb97e47bffd203e7afdeb2ab1c
gimunkwon <gimon71@gmail.com>
2026-02-13 13:28:29 +0900

    None

--- Source/CampMiniProject/Private/Global/Gamemode/MiniGameMode.cpp
+++ Source/CampMiniProject/Private/Global/Gamemode/MiniGameMode.cpp
@@ -1,4 +1,1 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Global/Gamemode/MiniGameMode.h"
+﻿#include "Global/Gamemode/MiniGameMode.h"
--- Source/CampMiniProject/Public/Global/Gamemode/MiniGameMode.h
+++ Source/CampMiniProject/Public/Global/Gamemode/MiniGameMode.h
@@ -1,14 +1,10 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "MiniGameMode.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class CAMPMINIPROJECT_API AMiniGameMode : public AGameModeBase
 {
