f169dd383c90d20d6d01f7b7edee601e8b2879bf
gimunkwon <gimon71@gmail.com>
2026-02-13 13:20:02 +0900

    Feat: 플레이의 Camera,SpringArm 생성및 세부 파라미터 조정, 캐릭터의 이동 구현

---
+++ Source/CampMiniProject/Private/GameActor/Character/BaseCharacter.cpp
@@ -0,0 +1,51 @@
+﻿#include "GameActor/Character/BaseCharacter.h"
+
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+#pragma region Camera,SpringArm
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength = 600.f;
+	SpringArmComp->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	SpringArmComp->bUsePawnControlRotation = false;
+	SpringArmComp->bDoCollisionTest = false;
+	SpringArmComp->bInheritPitch = false;
+	SpringArmComp->bInheritRoll = false;
+	SpringArmComp->bInheritYaw = false;
+	
+	TopDownCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	TopDownCamera->SetupAttachment(SpringArmComp);
+	TopDownCamera->bUsePawnControlRotation = false;
+#pragma endregion 
+}
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ABaseCharacter::MoveToLocation(FVector& TargetLocation)
+{
+	FVector StartLocation = GetActorLocation();
+	FVector EndLocation = TargetLocation;
+	
+	FVector GoalLocation = TargetLocation - StartLocation;
+	GoalLocation.Z = 0.f;
+	
+	UAIBlueprintHelperLibrary::SimpleMoveToLocation(GetController(),GoalLocation);
+	
+}
+
+
+
+
+
+
---
+++ Source/CampMiniProject/Public/GameActor/Character/BaseCharacter.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class CAMPMINIPROJECT_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+	
+#pragma region Movement
+	void MoveToLocation(FVector& TargetLocation);
+#pragma endregion
+	
+protected:
+	virtual void BeginPlay() override;
+	
+#pragma region Camera,SpringArm
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	TObjectPtr<UCameraComponent> TopDownCamera;
+	UPROPERTY(VisibleAnywhere, Category="Camera|SpringArm")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+#pragma endregion
+	
+};
