ee7677c5b179127dbc34e7081493d45fded0eab1
gimunkwon <gimon71@gmail.com>
2026-02-13 13:29:34 +0900

    Feat: 현재 캐릭터의 Velocity값을 애님인스턴스에 매 틱마다 갱신을 해서 향후 적용할 BlendSpace의 가로 축 값의 파라미터로 사용할수있게함

---
+++ Source/CampMiniProject/Private/GameActor/Animations/BasePlayerAnimInst.cpp
@@ -0,0 +1,19 @@
+﻿#include "GameActor/Animations/BasePlayerAnimInst.h"
+
+#include "GameActor/Character/BaseCharacter.h"
+
+void UBasePlayerAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	OwnerCharacter = Cast<ABaseCharacter>(TryGetPawnOwner());
+}
+
+void UBasePlayerAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!OwnerCharacter) return;
+	
+	GroundSpeed = OwnerCharacter->GetVelocity().Size2D();
+}
---
+++ Source/CampMiniProject/Public/GameActor/Animations/BasePlayerAnimInst.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BasePlayerAnimInst.generated.h"
+
+
+class ABaseCharacter;
+
+UCLASS()
+class CAMPMINIPROJECT_API UBasePlayerAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Movement|Speed")
+	float GroundSpeed;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<ABaseCharacter> OwnerCharacter;
+};
