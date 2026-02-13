7dbf7b048508fdfe4db88a5fa342e68d00b809c1
gimunkwon <gimon71@gmail.com>
2026-02-13 17:24:08 +0900

    Feat: NPC의 USphereComponent생섬및OnComponentBeginOverlap,OnComponentEndOverlap 에사용자정의 함수 바인딩

---
+++ Source/CampMiniProject/Public/GameActor/Character/NPC/BaseNPC.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameActor/Character/Player/BaseCharacter.h"
+#include "BaseNPC.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class CAMPMINIPROJECT_API ABaseNPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseNPC();
+protected:
+	virtual void BeginPlay() override;
+	UPROPERTY(EditDefaultsOnly,Category="Collision|Interaction")
+	TObjectPtr<USphereComponent> InteractionSphere;
+	
+	
+	// 접근 감지시
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+						bool bFromSweep, const FHitResult& SweepResult);
+	
+	// 접근 해제시
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+					  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
