564bbbb7852d978cb7a8ce0dec56b8e8d0fa8bf0
gimunkwon <gimon71@gmail.com>
2026-02-13 17:25:27 +0900

    Feat: NPCWidget의 텍스트블록NPCText 바인딩 및 SetNPCText 함수를 추가해 해당 텍스트 블록의 텍스트를 변경하도록 로직 추가

---
+++ Source/CampMiniProject/Private/UI/NPC/Widget/NPCWidget.cpp
@@ -0,0 +1,11 @@
+﻿#include "UI/NPC/Widget/NPCWidget.h"
+
+#include "Components/TextBlock.h"
+
+void UNPCWidget::SetNPCText()
+{
+	if (NPCText)
+	{
+		NPCText->SetText(FText::FromString(FString::Printf(TEXT("내가 던전 문을 열어 주도록 하지"))));
+	}
+}
---
+++ Source/CampMiniProject/Public/UI/NPC/Widget/NPCWidget.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NPCWidget.generated.h"
+
+class UTextBlock;
+
+UCLASS()
+class CAMPMINIPROJECT_API UNPCWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	void SetNPCText();
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> NPCText;
+};
