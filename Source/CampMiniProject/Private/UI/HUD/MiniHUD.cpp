397558539a9b7958c99bf82d8bce40efbebce1cc
gimunkwon <gimon71@gmail.com>
2026-02-16 21:47:07 +0900

    None

--- Source/CampMiniProject/Private/UI/HUD/MiniHUD.cpp
+++ Source/CampMiniProject/Private/UI/HUD/MiniHUD.cpp
@@ -1,29 +1,4 @@
 ﻿#include "UI/HUD/MiniHUD.h"
 
-#include "Blueprint/UserWidget.h"
-#include "GameActor/Character/Player/BaseCharacter.h"
-#include "UI/NPC/Widget/NPCWidget.h"
 
 
-void AMiniHUD::ShowNPCWidget()
-{
-	if (NPCWidgetClass)
-	{
-		NPCWidget = CreateWidget<UNPCWidget>(GetWorld(),NPCWidgetClass);
-		NPCWidget->SetNPCText();
-		NPCWidget->AddToViewport();
-		
-		if (ABaseCharacter* BaseCharacter = Cast<ABaseCharacter>(GetOwningPlayerController()->GetPawn()))
-		{
-			BaseCharacter->SetIsConnectedNPC(false);
-		}
-		
-		
-		FTimerHandle WidgetTimerHandle;
-		
-		GetWorld()->GetTimerManager().SetTimer(WidgetTimerHandle, [this]()
-		{
-			if (NPCWidget->IsInViewport()) NPCWidget->RemoveFromParent();
-		},3.f, false);
-	}
-}
--- Source/CampMiniProject/Public/UI/HUD/MiniHUD.h
+++ Source/CampMiniProject/Public/UI/HUD/MiniHUD.h
@@ -11,14 +11,4 @@ UCLASS()
 class CAMPMINIPROJECT_API AMiniHUD : public AHUD
 {
 	GENERATED_BODY()
-	
-public:
-	void ShowNPCWidget();
-	
-protected:
-	UPROPERTY(EditAnywhere,Category="Widget|NPC")
-	TSubclassOf<UUserWidget> NPCWidgetClass;
-	
-	UPROPERTY()
-	TObjectPtr<UNPCWidget> NPCWidget;
 };
