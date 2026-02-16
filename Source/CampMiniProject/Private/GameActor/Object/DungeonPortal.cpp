2af6533e53861c6b2681d611cb540329f173ba6e
gimunkwon <gimon71@gmail.com>
2026-02-16 21:52:18 +0900

    Feat: 캐릭터와 Collision 충돌감지로 상호작용을 할수있는 Actor Class DungeonPortal 추가

---
+++ Source/CampMiniProject/Private/GameActor/Object/DungeonPortal.cpp
@@ -0,0 +1,87 @@
+﻿#include "GameActor/Object/DungeonPortal.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Components/SphereComponent.h"
+#include "GameActor/Character/Player/BaseCharacter.h"
+#include "UI/Object/Portal/PortalWidget.h"
+
+
+ADungeonPortal::ADungeonPortal()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+#pragma region RootComponent
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+#pragma endregion 
+	
+#pragma region Mesh
+	DefaultMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	DefaultMesh->SetupAttachment(RootComponent);
+#pragma endregion
+	
+#pragma region Collision
+	InteractionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("InteractionSphere"));
+	InteractionSphere->SetSphereRadius(400.f);
+	InteractionSphere->SetupAttachment(DefaultMesh);
+#pragma endregion 
+	
+#pragma region Niagara
+	PortalEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PortalEffect"));
+	PortalEffect->SetupAttachment(RootComponent);
+#pragma endregion 
+	
+}
+
+
+void ADungeonPortal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InteractionSphere->OnComponentBeginOverlap.AddDynamic(this, &ADungeonPortal::OnOverlapBegin);
+	InteractionSphere->OnComponentEndOverlap.AddDynamic(this, &ADungeonPortal::OnOverlapEnd);
+}
+
+void ADungeonPortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp,Warning,TEXT("던전 입구에 접근했습니다 E를 눌러 상호작용 하세요"));
+	
+	if (ABaseCharacter* BasePlayer = Cast<ABaseCharacter>(OtherActor))
+	{
+		BasePlayer->SetInteractiveActor(this);
+	}
+	
+	bIsOpenWidget = true;
+	
+}
+
+void ADungeonPortal::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp,Warning,TEXT("던전 입구근처에 플레이어 없음"));
+	
+	if (PortalWidget)
+	{
+		PortalWidget->RemoveFromParent();
+		bIsOpenWidget = false;
+	}
+}
+
+void ADungeonPortal::OpenDungeonEnterWidget()
+{
+	if (bIsOpenWidget)
+	{
+		bIsOpenWidget = false;
+		
+		PortalWidget = CreateWidget<UPortalWidget>(GetWorld(),PortalWidgetClass);
+		
+		if (PortalWidget)
+		{
+			PortalWidget->AddToViewport();
+		}
+		
+	}
+}
+
+
+
---
+++ Source/CampMiniProject/Public/GameActor/Object/DungeonPortal.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraComponent.h"
+#include "DungeonPortal.generated.h"
+
+class UPortalWidget;
+class USphereComponent;
+
+UCLASS()
+class CAMPMINIPROJECT_API ADungeonPortal : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADungeonPortal();
+	
+	void OpenDungeonEnterWidget();
+	
+protected:
+	virtual void BeginPlay() override;
+protected:
+	UPROPERTY(EditAnywhere,Category="Mesh")
+	TObjectPtr<UStaticMeshComponent> DefaultMesh;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Collision|Interaction")
+	TObjectPtr<USphereComponent> InteractionSphere;
+	
+#pragma region NiagaraComponent
+	UPROPERTY(VisibleAnywhere,Category="Niagara|Components")
+	TObjectPtr<UNiagaraComponent> PortalEffect;
+	
+	UPROPERTY(EditAnywhere,Category="Niagara|Effects")
+	TObjectPtr<UNiagaraSystem> PortalSystem;
+#pragma endregion 
+	
+#pragma region CollisionEvent
+	// 접근 감지시
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+						bool bFromSweep, const FHitResult& SweepResult);
+	
+	// 접근 해제시
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+					  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+#pragma endregion
+	
+#pragma region Widget
+	UPROPERTY(EditAnywhere,Category="Widget")
+	TSubclassOf<UUserWidget> PortalWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UPortalWidget> PortalWidget;
+#pragma endregion 
+
+private:
+	bool bIsOpenWidget = false;
+};
