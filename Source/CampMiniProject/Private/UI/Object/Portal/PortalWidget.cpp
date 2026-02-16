b99919d3ba36f0f21e6a51f6abe2fdbdd955461c
gimunkwon <gimon71@gmail.com>
2026-02-16 23:46:31 +0900

    PortalWidget의 BindWidget Enter_Btn과 Close_Btn이 눌러졌을때 블루프린트에서 호출될 함수들을 정의

---
+++ Source/CampMiniProject/Private/UI/Object/Portal/PortalWidget.cpp
@@ -0,0 +1,28 @@
+﻿#include "UI/Object/Portal/PortalWidget.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void UPortalWidget::OnEnterButton()
+{
+	if (Enter_Btn)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("EnterButton ON"));
+		
+		if (KamenRaidLevelName.IsNone())
+		{
+			UE_LOG(LogTemp,Warning,TEXT("Level Name Is Not set"));
+			return;
+		}
+		
+		UGameplayStatics::OpenLevel(this, KamenRaidLevelName);
+		RemoveFromParent();
+	}
+}
+
+void UPortalWidget::OnCloseButton()
+{
+	if (Close_Btn)
+	{
+		RemoveFromParent();
+	}
+}
---
+++ Source/CampMiniProject/Public/UI/Object/Portal/PortalWidget.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PortalWidget.generated.h"
+
+
+class UButton;
+
+UCLASS()
+class CAMPMINIPROJECT_API UPortalWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere,Category="Level")
+	FName KamenRaidLevelName;
+	
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Enter_Btn;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Close_Btn;
+	
+	// BlueprintCallable Function
+	UFUNCTION(BlueprintCallable,Category="Button|Enter")
+	void OnEnterButton();
+	UFUNCTION(BlueprintCallable,Category="Button|Close")
+	void OnCloseButton();
+};
