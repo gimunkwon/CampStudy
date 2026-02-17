89f12a461362312599b477adc950a9b319193407
gimunkwon <gimon71@gmail.com>
2026-02-17 17:05:03 +0900

    Feat: 몬스터 클래스 생성및 InteractiveMesh를 통해 플레이어의 Collision충돌 여부를 감지해 해당 이벤트를 재생하는 로직추가

---
+++ Source/CampMiniProject/Private/GameActor/Character/Enemy/BaseEnemey.cpp
@@ -0,0 +1,93 @@
+﻿#include "GameActor/Character/Enemy/BaseEnemey.h"
+
+#include "Blueprint/UserWidget.h"
+#include "Components/SphereComponent.h"
+#include "GameActor/Character/Player/BaseCharacter.h"
+
+
+ABaseEnemey::ABaseEnemey()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(GetMesh(), TEXT("WeaponSocket"));
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	InteractiveMesh = CreateDefaultSubobject<USphereComponent>(TEXT("InteractiveMesh"));
+	InteractiveMesh->SetupAttachment(RootComponent);
+	InteractiveMesh->SetSphereRadius(100.f);
+	InteractiveMesh->SetRelativeLocation(FVector(150.0f, 0.0f, 0.f));
+	
+}
+
+
+void ABaseEnemey::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InteractiveMesh->OnComponentBeginOverlap.AddDynamic(this, &ABaseEnemey::OnOverlapBegin);
+	InteractiveMesh->OnComponentEndOverlap.AddDynamic(this, &ABaseEnemey::OnOverlapEnd);
+}
+
+void ABaseEnemey::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp,Warning,TEXT("%s 범위내에 들어왔습니다 격돌 상호작용을 하십시오"),*OtherActor->GetName());
+	
+	if (ClashToolTipWidgetClass)
+	{
+		ClashToolTipWidget = CreateWidget(GetWorld(),ClashToolTipWidgetClass);
+		if (ClashToolTipWidget)
+		{
+			ClashToolTipWidget->AddToViewport();
+		}
+	}
+	
+	
+	if (ABaseCharacter* BasePlayer = Cast<ABaseCharacter>(OtherActor))
+	{
+		BasePlayer->SetClashActor(this);
+	}
+	
+}
+
+void ABaseEnemey::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if (ClashToolTipWidget)
+	{
+		ClashToolTipWidget->RemoveFromParent();
+	}
+	
+	UE_LOG(LogTemp,Warning,TEXT("%s 범위에서 나감"),*OtherActor->GetName());
+}
+
+void ABaseEnemey::PlayEnemyClash()
+{
+	if (ClashToolTipWidget)
+	{
+		ClashToolTipWidget->RemoveFromParent();
+	}
+	
+	InteractiveMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	if (UAnimInstance* BaseAnimInstance = GetMesh()->GetAnimInstance())
+	{
+		if (EnemyClashMontage_01)
+		{
+			BaseAnimInstance->Montage_Play(EnemyClashMontage_01);
+		}
+		
+	}
+}
+
+void ABaseEnemey::EndEnemyClash()
+{
+	FTimerHandle DestroyTimer;
+	
+	GetWorld()->GetTimerManager().SetTimer(DestroyTimer, [this]()
+	{
+		this->Destroy();
+	},4.f,false);
+
+}	
+
---
+++ Source/CampMiniProject/Public/GameActor/Character/Enemy/BaseEnemey.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseEnemey.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class CAMPMINIPROJECT_API ABaseEnemey : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseEnemey();
+	
+	void PlayEnemyClash();
+	void EndEnemyClash();
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere,Category="Mesh|Weapon")
+	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+	
+	// QTE를 감지할 영역
+	UPROPERTY(EditAnywhere,Category="Collision|Interactive")
+	TObjectPtr<USphereComponent> InteractiveMesh;
+	
+#pragma region CollisionEvent
+	// 접근 감지시
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+						bool bFromSweep, const FHitResult& SweepResult);
+	
+	// 접근 해제시
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+					  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+#pragma endregion
+	
+#pragma region Animations
+	UPROPERTY(Editanywhere,Category="Animation|AnimMontage")
+	TObjectPtr<UAnimMontage> EnemyClashMontage_01;
+#pragma endregion
+	
+#pragma region Widget
+	UPROPERTY(EditAnywhere,Category="Widget")
+	TSubclassOf<UUserWidget> ClashToolTipWidgetClass;
+	UPROPERTY()
+	TObjectPtr<UUserWidget> ClashToolTipWidget;
+#pragma endregion
+};
