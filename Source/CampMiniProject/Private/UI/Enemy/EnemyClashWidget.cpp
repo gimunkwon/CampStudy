4e35b67987c531e0c9b8bbddea63c1fc05e35650
gimunkwon <gimon71@gmail.com>
2026-02-17 17:01:10 +0900

    Feat: 해당 QTE액션 위젯의 SetVisibility 값을 조정하기위한 Getter,Setter함수 추가

---
+++ Source/CampMiniProject/Private/UI/Enemy/EnemyClashWidget.cpp
@@ -0,0 +1,35 @@
+﻿#include "UI/Enemy/EnemyClashWidget.h"
+
+#include "GameActor/Controller/BaseCharacterController.h"
+
+void UEnemyClashWidget::HideQ_Widet()
+{
+	if (WBP_Clash_Q)
+	{
+		WBP_Clash_Q->SetVisibility(ESlateVisibility::Collapsed);
+		bIsHideQ_Widget = true;
+		CheckClashCompletion();
+	}
+}
+
+void UEnemyClashWidget::HideW_Widget()
+{
+	if (WBP_Clash_W)
+	{
+		WBP_Clash_W->SetVisibility(ESlateVisibility::Collapsed);
+		bIsHideW_Widget = true;
+		CheckClashCompletion();
+	}
+}
+
+void UEnemyClashWidget::CheckClashCompletion()
+{
+	if (bIsHideQ_Widget && bIsHideW_Widget)
+	{
+		if (auto* PC = Cast<ABaseCharacterController>(GetOwningPlayer()))
+		{
+			PC->ResumeEnemyAnimation();
+		}
+	}
+}
+
---
+++ Source/CampMiniProject/Public/UI/Enemy/EnemyClashWidget.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyClashWidget.generated.h"
+
+UCLASS()
+class CAMPMINIPROJECT_API UEnemyClashWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void HideQ_Widet();
+	void HideW_Widget();
+	
+	void ShowW_Widget();
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget> WBP_Clash_Q;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UUserWidget> WBP_Clash_W;
+	
+private:
+	void CheckClashCompletion();
+	
+	bool bIsHideQ_Widget = false;
+	bool bIsHideW_Widget = false;
+};
