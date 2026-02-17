0de747bdc22b8254413c46953514912d9d202db3
gimunkwon <gimon71@gmail.com>
2026-02-17 16:59:50 +0900

    None

---
+++ Source/CampMiniProject/Private/GameActor/Animations/Enemy/AnimnotifyState/EnemyAnimNotifyState.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameActor/Animations/Enemy/AnimnotifyState/EnemyAnimNotifyState.h"
+
+void UEnemyAnimNotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+	
+	
+}
---
+++ Source/CampMiniProject/Public/GameActor/Animations/Enemy/AnimnotifyState/EnemyAnimNotifyState.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "EnemyAnimNotifyState.generated.h"
+
+UCLASS()
+class CAMPMINIPROJECT_API UEnemyAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+};
